deac6a060546c0e44ac87d4970e78fe45cbf92a0
Your Name <you@example.com>
2022-09-15 16:19:03 -0400

    last

--- models/user.py
+++ models/user.py
@@ -5,8 +5,12 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.review import Review
 from models.place import Place
+from os import getenv
+
 
 storage_type=getenv("HBNB_TYPE_STORAGE")
+
+
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"

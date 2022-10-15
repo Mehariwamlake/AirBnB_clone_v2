629433773904e88e3b227b2f3b5a7d8f178ea449
Your Name <you@example.com>
2022-10-15 13:14:06 -0400

    last

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-from fabric.api import *
-import os
+"""
+    Fabric script that generates tgz archive from contents of web_static
+"""
+from fabric.api import local
 from datetime import datetime
-
-env.hosts = ['localhost']
 
 
 def do_pack():
+    """
+        generates a .tgz archine from contents of web_static
+    """
+    time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+    file_name = "versions/web_static_{}.tgz".format(time)
     try:
-        filepath = "versions/web_static_" + datetime.now().\
-                   strftime("%Y%m%d%H%M%S") + ".tgz"
-        local("mkdir -p versions")
-        local("tar -zcvf versions/web_static_$(date +%Y%m%d%H%M%S).tgz\
-        web_static")
-        print("web_static packed: {} -> {}".
-              format(filepath, os.path.getsize(filepath)))
+        local("mkdir -p ./versions")
+        local("tar --create --verbose -z --file={} ./web_static"
+              .format(file_name))
+        return file_name
     except:
-            return None
+        return None
